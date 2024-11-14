7b682a5e9072024a36ff37f478985478b3558448
AndrePhrecius <andre.piloto17@yahoo.com.br>
2024-11-13 21:14:59 -0300

    Initial commit

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public AssetReference assetReference;
+    AsyncOperationHandle<GameObject> handle;
+    List<GameObject> prefabs = new List<GameObject>();
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (prefabs.Count <= 0) LoadAsset();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (prefabs.Count > 0) UnloadAsset();
+        }
+    }
+
+    void LoadAsset()
+    {
+        handle = assetReference.InstantiateAsync(transform.position, Quaternion.identity);
+        handle.Completed += handle => { prefabs.Add(handle.Result); };
+    }
+
+    void UnloadAsset()
+    {
+        foreach (GameObject prefab in prefabs)
+        {
+            Addressables.ReleaseInstance(prefab);
+        }
+        prefabs.Clear();
+    }
+
+}
